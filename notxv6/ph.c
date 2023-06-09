68ff806a75e73f258815cd461bd4ba9361033239
Blue233333 <1552095362@qq.com>
2023-06-09 10:00:13 +0800

    hash

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,7 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
-pthread_mutex_t lock;
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -53,9 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
-    pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -121,7 +121,8 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
-  pthread_mutex_init(&lock, NULL);
+  for (int i = 0; i < NBUCKET; i++)
+    pthread_mutex_init(&lock[i], NULL);
   //
   // first the puts
   //
